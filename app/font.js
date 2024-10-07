83a6b938463531cb022caa97b758a910432989b0
unknown <dpk.negircks@gmail.com>
2024-10-07 12:13:15 -0700

    added info div

--- app/font.js
+++ app/font.js
@@ -1,10 +1,19 @@
-import { Libre_Baskerville, Permanent_Marker } from "next/font/google";
+import {
+  Libre_Baskerville,
+  Permanent_Marker,
+  DM_Serif_Display,
+} from "next/font/google";
 const libre = Libre_Baskerville({ subsets: ["latin"], weight: ["400", "700"] });
 const marker = Permanent_Marker({ subsets: ["latin"], weight: ["400"] });
+const dm_serif_display = DM_Serif_Display({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 const customFonts = {
   libre,
   marker,
+  dm_serif_display,
 };
 
 export default customFonts;
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import { DatePicker } from "@/components/DatePicker";
 import ImageSlider from "@/components/ImageSlider";
 import { Button } from "@/components/ui/button";
 import { FaArrowRight } from "react-icons/fa";
+import customFonts from "./font";
 
 export default function Home() {
   return (
@@ -22,6 +23,33 @@ export default function Home() {
       </div>
 
       {/** content for introduction */}
+      <div className="flex flex-col py-2 my-4 items-center justify-center gap-3">
+        <div
+          className={`font-semibold text-sm sm:text-xl ${customFonts.dm_serif_display.className}`}
+        >
+          Neque porro quisquam est qui dolorem ipsum quia dolor sit ame
+        </div>
+        <div className="flex flex-col items-center justify-center bg-[#ebebeb] mx-10 my-4 w-[500px] sm:w-[800px] shadow-lg rounded-lg p-4">
+          <div className={`text-sm sm:text-lg ${customFonts.libre.className}`}>
+            This is demo text. content will be added
+          </div>
+          <p
+            className={`p-4 text-sm sm:text-lg ${customFonts.libre.className}`}
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged.
+          </p>
+          <Button variant="outline">
+            Discover <FaArrowRight className="ml-2" />{" "}
+          </Button>
+        </div>
+      </div>
+
+      {/** carousel for images */}
     </>
   );
 }
